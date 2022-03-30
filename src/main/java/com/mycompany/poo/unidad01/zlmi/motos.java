d203a49b3ac59455d2b5e4a7094f3e66b6876bbf
PC-11 <PC-11@DESKTOP-OLVLIFE>
2022-03-30 14:52:28 +0200

    cambio mensaje de hola mundo a español. se agrego otra linea de codigo diciendo awa se agrego una clase se agrego la clase de motos

---
+++ src/main/java/com/mycompany/poo/unidad01/zlmi/motos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo.unidad01.zlmi;
+
+/**
+ *
+ * @author PC-11
+ */
+public class motos {
+    String marca;
+    int placa;
+    int año;
+    String modelo;
+    double precio;
+}
