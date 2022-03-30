cceaedae1d60f5699f8deb83c73e9f1b02e5ab07
PC-11 <PC-11@DESKTOP-OLVLIFE>
2022-03-30 13:38:47 +0200

    cambio mensaje de hola mundo a español.

---
+++ src/main/java/com/mycompany/poo/unidad01/zlmi/PooUnidad01ZLMI.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo.unidad01.zlmi;
+
+/**
+ *
+ * @author PC-11
+ */
+public class PooUnidad01ZLMI {
+
+    public static void main(String[] args) {
+        System.out.println("Aaa!");
+        
+    }
+}
