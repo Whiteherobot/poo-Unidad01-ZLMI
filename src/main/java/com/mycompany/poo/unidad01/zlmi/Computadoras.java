7e4097802fbea73566a4cfcf8133bd1273aa7f2e
ealex <ealex@DESKTOP-5T1U4JC>
2022-03-31 15:28:33 -0500

    Tarea N1; creacion de objetos; clases para el entendimiento de que es lo que trata

---
+++ src/main/java/com/mycompany/poo/unidad01/zlmi/Computadoras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo.unidad01.zlmi;
+
+/**
+ *
+ * @author Michi
+ */
+public class Computadoras {
+    public String Componentes;
+    public double Costos;
+    public String marca;
+    public float capacidad;
+}
